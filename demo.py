4f47391591ff2d3cc914442697816dacebd4ef08
Undergrad-Sarthak <sarthakj337@gmail.com>
2024-08-06 22:29:04 +0530

    .

--- demo.py
+++ demo.py
@@ -21,10 +21,8 @@ print("3.Multiply")
 print("4.Divide")
 
 while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice: ")
 
-    # check if choice is one of the four options
     if choice in ("1", "2", "3", "4"):
         try:
             num1 = float(input("Enter first number: "))
@@ -45,8 +43,6 @@ while True:
         elif choice == "4":
             print(num1, "/", num2, "=", div(num1, num2))
 
-        # check if user wants another calculation
-        # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
             break
